0e8c6094321c1596dfd6c441551fea1a3ecdc2d3
Fernando <engffsantos@gmail.com>
2025-01-31 09:22:58 -0300

    Atualização do requirements.txt

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
+import os
 from flask_migrate import Migrate
-
 from app.__init__ import create_app, db
 
 app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(debug=app.config.get('DEBUG', False))
+    port = int(os.environ.get("PORT", 8000))  # Pega a porta definida pelo Azure ou usa 8000 como padrão
+    app.run(host="0.0.0.0", port=port, debug=app.config.get('DEBUG', False))
